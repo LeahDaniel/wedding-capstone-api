5c8056da526942e3994c63efb396f8738222ee90
LeahDaniel <leah.daniel1997@gmail.com>
2022-03-23 11:50:36 -0500

    example images complete

--- seed_data.sh
+++ seed_data.sh
@@ -2,4 +2,4 @@ rm db.sqlite3
 ./manage.py migrate
 ./manage.py loaddata vendorTypes weddingSizes
 ./manage.py seed_db
-./manage.py loaddata visionPhotos
+./manage.py loaddata visionPhotos exampleImages
